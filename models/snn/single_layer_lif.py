5b5acc8bbcf54162d54225a01807251fcbbf5ae0
Denys Savenkov <dsavenkov@datrics.ai>
2023-04-02 21:07:56 +0300

    First prototypes in data drift detection

--- models/snn/single_layer_lif.py
+++ models/snn/single_layer_lif.py
@@ -13,7 +13,10 @@ class SingleLayerLIF(nn.Module):
         self.layers = nn.Sequential(
             layer.Flatten(),
             layer.Linear(n_input, n_output, bias=False),
-            neuron.LIFNode(tau=tau, surrogate_function=surrogate.ATan())
+            neuron.ParametricLIFNode(init_tau=tau,
+                                     # v_threshold=1,v_reset=-10,
+                                     # surrogate_function=surrogate.ATan()
+                                     )
         )
 
     def forward(self, x: torch.Tensor):
